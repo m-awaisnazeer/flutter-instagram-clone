6bfae4fb0640d04bb08bf0388869a4d11a58fe14
Awais <m.awaisnaxeer@gmail.com>
2022-05-02 16:01:13 +0500

    Firebase Setup for Android and Web

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,26 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_flutter_clone/responsive/mobile_screen_layout.dart';
 import 'package:instagram_flutter_clone/responsive/responsive_layout_screen.dart';
 import 'package:instagram_flutter_clone/responsive/web_screen_layout.dart';
 import 'package:instagram_flutter_clone/utils/colors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding
+      .ensureInitialized(); //firebase init.. even before rendering widgets, so we ensure widgets got comes in before firebase
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+      apiKey: 'AIzaSyBwKYCWXdqd8avS0RW7Ei_zaKdiD_weHLg',
+      appId: '1:85225426577:web:15d4314964502faf71b930',
+      messagingSenderId: '85225426577',
+      projectId: 'flutter-instagram-clone-e0af2',
+      storageBucket: "flutter-instagram-clone-e0af2.appspot.com",
+    ));
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
