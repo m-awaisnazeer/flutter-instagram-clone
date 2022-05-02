567a2c22d95acfed3681b60ce0722febfdc06123
Awais <m.awaisnaxeer@gmail.com>
2022-05-02 15:01:17 +0500

    Responsive Layouts created for web and mobile and firebase dependencies also created

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_flutter_clone/responsive/mobile_screen_layout.dart';
+import 'package:instagram_flutter_clone/responsive/responsive_layout_screen.dart';
+import 'package:instagram_flutter_clone/responsive/web_screen_layout.dart';
+import 'package:instagram_flutter_clone/utils/colors.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +17,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Instagram Clone',
-      home: Text("Lets Build Instagram Clone Together"),
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: mobileBackgroundColor,
+      ),
+      home: const ResponsiveLayout(
+        mobileScreenLayout: MobileScreenLayout(),
+        webScreenLayout: WebScreenLayout(),
+      ),
     );
   }
 }
