c7c6a6a469c2b3a017ef9c41e61686444f2466c9
Awais <m.awaisnaxeer@gmail.com>
2022-05-05 08:22:37 +0500

    Login Screen Designed

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:instagram_flutter_clone/responsive/mobile_screen_layout.dart';
 import 'package:instagram_flutter_clone/responsive/responsive_layout_screen.dart';
 import 'package:instagram_flutter_clone/responsive/web_screen_layout.dart';
+import 'package:instagram_flutter_clone/screens/login_screen.dart';
 import 'package:instagram_flutter_clone/utils/colors.dart';
 
 void main() async {
@@ -36,10 +37,11 @@ class MyApp extends StatelessWidget {
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: mobileBackgroundColor,
       ),
-      home: const ResponsiveLayout(
-        mobileScreenLayout: MobileScreenLayout(),
-        webScreenLayout: WebScreenLayout(),
-      ),
+      // home: const ResponsiveLayout(
+      //   mobileScreenLayout: MobileScreenLayout(),
+      //   webScreenLayout: WebScreenLayout(),
+      // ),
+      home: LoginScreen(),
     );
   }
 }
