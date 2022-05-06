b5c2d1ddc9cbc5400bf8013ae3c1688476e8199a
Awais <m.awaisnaxeer@gmail.com>
2022-05-06 16:18:57 +0500

    User model class created

--- lib/resources/auth_methods.dart
+++ lib/resources/auth_methods.dart
@@ -2,7 +2,7 @@ import 'dart:typed_data';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
+import 'package:instagram_flutter_clone/models/User.dart' as model;
 import 'package:instagram_flutter_clone/resources/storage_methods.dart';
 
 class AuthMethods {
@@ -30,15 +30,18 @@ class AuthMethods {
         String photoUrl = await StorageMethods()
             .uploadImageToStorage("profilePics", file, false);
         // add user to database
-        await _firestore.collection('users').doc(credential.user?.uid).set({
-          'username': username,
-          'uid': credential.user?.uid,
-          'email': email,
-          'bio': bio,
-          'followers': [],
-          'following': [],
-          'photoUrl': photoUrl,
-        });
+        model.User user = model.User(
+          email: email,
+          uid: credential.user!.uid,
+          photoUrl: photoUrl,
+          username: username,
+          bio: bio,
+          followers: [],
+          following: [],
+        );
+        await _firestore.collection('users').doc(credential.user?.uid).set(
+              user.toJson(),
+            );
 
         res = "Success";
       }
